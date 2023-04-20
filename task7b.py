1ebefe04ef0f0def23cfc642f81b8c724a59dbb9
hamna noor <hamna@rakurai.io>
2023-04-20 16:00:32 +0500

    fixed errors

--- task7b.py
+++ task7b.py
@@ -5,11 +5,12 @@ import logging
 def divisbility_check(dividend, divisor):
     
     logging.info(f"finding divisible numbers in range 1 -{dividend} by {divisor}")
+    count = 0
     for i in range(1,dividend+1):
         if i%divisor==0:
             count+=1
     logging.info(f"Found {count} divisible numbers in range 1-{dividend} by {divisor}")
-logging.basicConfig(filename='async_divisibles.log', level=logging.INFO)
+logging.basicConfig(filename='divisibles.log', level=logging.INFO)
 logging.info(f" <----SYNCRONOUS---->")
 start_time =time.time()
 divisbility_check(50800000, 3411)
