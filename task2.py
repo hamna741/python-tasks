5b97a63e463e8e58ad9c5545e02c495184628e3b
hamna noor <hamna@rakurai.io>
2023-04-20 15:31:19 +0500

    removed commented out lines of code

--- task2.py
+++ task2.py
@@ -1,27 +1,11 @@
-# import sys
-# import cpuinfo
-# print(sys.byteorder)
-
-# print(cpuinfo.get_cpu_info()['brand'])
-
-# import subprocess
-# import platform
-# #system_info=subprocess.check_output("lscpu", shell=True).strip().decode()
-
-# system_info= platform.uname()
-# print("BYTE ORDER",system_info.byteorder)
-
-#import platform
 import subprocess
 import distro
 system_info = subprocess.check_output("lscpu").decode("utf-8").splitlines()
-#print(system_info)
 core = [line.split(":")[1].strip() for line in system_info if "Core(s) per socket" in line][0]
 byte_order=[line.split(":")[1].strip() for line in system_info if "Byte Order" in line][0]
 model_name = [line.split(":")[1].strip() for line in system_info if "Model name" in line][0]
 cpu_max_freq = [line.split(":")[1].strip().split()[0] for line in system_info if "CPU max MHz" in line][0]
 cpu_min_freq = [line.split(":")[1].strip().split()[0] for line in system_info if "CPU min MHz" in line][0]
-#virtualization = True if "vmx" in system_info else False
 V1=[line.split(":")[1].strip().split()[0] for line in system_info if "Virtualization:" in line][0]
 l1i_cache_size = [line.split(":")[1].strip().split()[0] for line in system_info if "L1i cache" in line][0]
 l1d_cache_size = [line.split(":")[1].strip().split()[0] for line in system_info if "L1d cache" in line][0]
@@ -31,7 +15,6 @@ thread_per_core = [line.split(":")[1].strip() for line in system_info if "Thread
 
 #     # Get distributor information
 distributor_id = distro.id()
-#distributor_description = distro.LinuxDistribution()
 distributor_codename = distro.codename()
 distribution = distro.linux_distribution()
 print("----------------SYSTEM INFORMATION--------------")
