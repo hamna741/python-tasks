22d7a59d5f98d53df8ca008144d23f3c328d4a2e
hamna noor <hamna@rakurai.io>
2023-04-20 15:36:59 +0500

    removed commented out lines of code

--- task5.py
+++ task5.py
@@ -1,13 +1,11 @@
 import argparse
 import os.path
-import subprocess
 import stat
 import permissions
 parser = argparse.ArgumentParser(description="executing linux command")
 parser.add_argument("file_name", type=str ,help="file path")
 arg=parser.parse_args()
-flag=os.path.isfile(arg.file_name)
-if flag is not True:
+if os.path.isfile(arg.file_name) is not True:
         with open(arg.file_name, 'w') as f:
             f.write('Create a new text file!')
             f.close()
@@ -15,7 +13,7 @@ current_permissions = os.stat(arg.file_name).st_mode
 
 # Remove all permissions from all users
 new_permissions = ~(stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
-#subprocess.run(["chmod a= /home/hamna/Downloads/dummy_file.txt"])
+
         
 os.chmod(arg.file_name,  0o000)
 try:
@@ -23,7 +21,7 @@ try:
 except (PermissionError) as e:
      print("read write permsion denied")
 
-finally:
-     #os.chmod('test.txt', stat.S_IRWXU|stat.S_IRWXG|stat.S_IRWXO)
-   permissions.change_permission(arg.file_name)
-   print("permissions reset")
+
+     
+permissions.change_permission(arg.file_name)
+print("permissions reset")
