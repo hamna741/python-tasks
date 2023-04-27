61fd82dbb9448badfc6754dcb072457f10100104
hamna noor <hamna@rakurai.io>
2023-04-27 18:56:57 +0500

    implementation using single for loop

--- task2.py
+++ task2.py
@@ -1,33 +1,47 @@
 import subprocess
 import distro
 system_info = subprocess.check_output("lscpu").decode("utf-8").splitlines()
-core = [line.split(":")[1].strip() for line in system_info if "Core(s) per socket" in line][0]
-byte_order=[line.split(":")[1].strip() for line in system_info if "Byte Order" in line][0]
-model_name = [line.split(":")[1].strip() for line in system_info if "Model name" in line][0]
-cpu_max_freq = [line.split(":")[1].strip().split()[0] for line in system_info if "CPU max MHz" in line][0]
-cpu_min_freq = [line.split(":")[1].strip().split()[0] for line in system_info if "CPU min MHz" in line][0]
-V1=[line.split(":")[1].strip().split()[0] for line in system_info if "Virtualization:" in line][0]
-l1i_cache_size = [line.split(":")[1].strip().split()[0] for line in system_info if "L1i cache" in line][0]
-l1d_cache_size = [line.split(":")[1].strip().split()[0] for line in system_info if "L1d cache" in line][0]
-l2_cache_size = [line.split(":")[1].strip().split()[0] for line in system_info if "L2 cache" in line][0]
-l3_cache_size = [line.split(":")[1].strip().split()[0] for line in system_info if "L3 cache" in line][0]
-thread_per_core = [line.split(":")[1].strip() for line in system_info if "Thread(s) per core" in line][0]
+
+
+for line in system_info:
+    if "Core(s) per socket" in line:
+        core=[line.split(":")[1].strip()][0]
+    if "Byte Order" in line:
+        byte_order=[line.split(":")[1].strip()][0]
+    if "Model name"  in line:
+        model_name=[line.split(":")[1].strip()][0]
+    if "CPU max MHz" in line:
+        CPU_max_MHz=[line.split(":")[1].strip()][0]
+    if "CPU min MHz" in line:
+        CPU_min_MHz=[line.split(":")[1].strip()][0]
+    if "L1i cache" in line:
+        L1i_cache=[line.split(":")[1].strip()][0]
+    if "L1d cache" in line:
+        L1d_cache=[line.split(":")[1].strip()][0]
+    if "L2 cache" in line:
+        L2_cache=[line.split(":")[1].strip()][0]
+    if "L3 cache" in line:
+        L3_cache=[line.split(":")[1].strip()][0]
+    if "Thread(s) per core" in line:
+        thread_per_core=[line.split(":")[1].strip()][0]
+    if "Virtualization:" in line:
+        V1=[line.split(":")[1].strip()][0]
 
 #     # Get distributor information
 distributor_id = distro.id()
-distributor_codename = distro.codename()
+# distributor_codename = distro.codename()
 distribution = distro.linux_distribution()
 print("----------------SYSTEM INFORMATION--------------")
 print("BYTE ORDER",byte_order)
 print("CORE: ",core)
 print("model_name  ",model_name)
-print("cpu_max_freq(MHz)  ",cpu_max_freq)
-print("cpu_min_freq(MHz) ",cpu_min_freq)
+print("cpu_max_freq(MHz)  ",CPU_max_MHz)
+print("cpu_min_freq(MHz) ",CPU_min_MHz)
 print("virtualization  ",V1)
-print("l1i_cache_size(KiB) ",l1i_cache_size )
-print("l1d_cache_size(KiB) ",l1d_cache_size )
-print("l2_cache_size(MiB)  ",l2_cache_size)
-print("l3_cache_size(MiB)  ",l3_cache_size)
+print("l1i_cache_size(KiB) ",L1i_cache )
+print("l1d_cache_size(KiB) ",L1d_cache )
+print("l2_cache_size(MiB)  ",L2_cache)
+print("l3_cache_size(MiB)  ",L3_cache)
 print("thread_per_core  ",thread_per_core)
 print("----------------DISTRIBUTOR INFORMATION--------------")
 print("distributor_id         ",distribution[0])
