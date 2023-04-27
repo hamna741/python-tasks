beb6426a76b0c7a541f09f869015b3b20920ea6b
hamna noor <hamna@rakurai.io>
2023-04-27 19:01:33 +0500

    implmented using narg arguments

--- task4.py
+++ task4.py
@@ -3,17 +3,17 @@ import math
 
 try:
     parser = argparse.ArgumentParser(description="Finding ditance between two coordinate pairs in cm ")
-    parser.add_argument("x1", type=float ,help="X cooridnate of first ordered pair in cm",)
-    parser.add_argument("y1", type=float ,help="y cooridnate of first ordered pair in cm",)
-    parser.add_argument("x2", type=float ,help="X cooridnate of second ordered pair in cm",)
-    parser.add_argument("y2", type=float ,help="y cooridnate of second ordered pair in cm",)
+    parser.add_argument("x1", nargs=2,type=float,
+                        help="X cooridnate of first ordered pair in cm",)
+    parser.add_argument("y1",nargs=2, type=float ,help="y cooridnate of first ordered pair in cm",)
+    #parser.add_argument("x2", type=float ,help="X cooridnate of second ordered pair in cm",)
+    #parser.add_argument("y2", type=float ,help="y cooridnate of second ordered pair in cm",)
     args = parser.parse_args()
 
-    x1=args.x1
-    y1=args.y1
-    x2=args.x2
-    y2=args.y2
-    distance =math.dist([x1,y1],[x2,y2])
+    pair1=args.x1
+    pair2=args.y1
+  
+    distance =math.dist(pair1,pair2)
     print(distance , "cm")
 
 except ValueError:
