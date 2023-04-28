6f60a5dff446436f49c1a26b99896810b55bed0e
hamna noor <hamna@rakurai.io>
2023-04-28 15:41:40 +0500

    fixed help infomation in add_argument

--- task4.py
+++ task4.py
@@ -3,18 +3,18 @@ import math
 
 try:
     parser = argparse.ArgumentParser(description="Finding ditance between two coordinate pairs in cm ")
-    parser.add_argument("x1", nargs=2,type=float,
-                        help="X cooridnate of first ordered pair in cm",)
-    parser.add_argument("y1",nargs=2, type=float ,help="y cooridnate of first ordered pair in cm",)
+    parser.add_argument("pair1", nargs=2,type=float,
+                        help="first coordinate pair",)
+    parser.add_argument("pair2",nargs=2, type=float ,help="second coordinate pair",)
     #parser.add_argument("x2", type=float ,help="X cooridnate of second ordered pair in cm",)
     #parser.add_argument("y2", type=float ,help="y cooridnate of second ordered pair in cm",)
     args = parser.parse_args()
 
-    pair1=args.x1
-    pair2=args.y1
+    pair1=args.pair1
+    pair2=args.pair2
   
     distance =math.dist(pair1,pair2)
-    print(distance , "cm")
+    print(round(distance,3) , "cm")
 
 except ValueError:
     print("invalid argument entered ")
